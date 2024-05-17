1c36960bbfdecfd757902fabee18a4b905198005
rustempro <rustemmustafin@yandex.ru>
2024-05-17 17:55:44 +0300

    questions1

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import styles from "./app.module.css";
+import "./App.css";
 
-function App() {
+export const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.app}>
+      <h1 className={styles["page-heading"]}>Ввод значения</h1>
+      <p className={["no-margin-text"]}>
+        Текущее значение <code>value</code>: "
+        <output className={"current-value"}></output>"
+      </p>
+      <div className={styles.error}>
+        Введенное значение должно содержать минимум 3 символа
+      </div>
+      <div className={styles["buttons-container"]}>
+        <button className={styles.button}>Ввести новое</button>
+        <button className={styles.button}>Добавить в список</button>
+      </div>
+      <div className={styles["list-container"]}>
+        <h2 className={styles["list-heading"]}>Список:</h2>
+        <p className={styles["no-margin-text"]}>Нет добавленных элементов</p>
+        <ul className={styles.list}>
+          <li className={styles["list-item"]}></li>
+        </ul>
+      </div>
     </div>
   );
-}
-
-export default App;
+};
