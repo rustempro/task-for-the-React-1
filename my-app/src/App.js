53a6573f42bf5e15b77886f853d657cb740d4b5c
rustempro <rustemmustafin@yandex.ru>
2024-05-18 15:09:34 +0300

    the finished result

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,27 +1,65 @@
 import styles from "./app.module.css";
-import "./App.css";
+import { useState } from "react";
 
 export const App = () => {
+  const [value, setValue] = useState("");
+  const [list, setList] = useState([]);
+  const [error, setError] = useState("");
+
+  const validate = (val) => val.length >= 3;
+
+  const onInputButtonClick = () => {
+    const promptValue = prompt("Введите значение");
+    if (validate(promptValue)) {
+      setValue(promptValue);
+      setError("");
+    } else {
+      setError("Введенное значение должно содержать минимум 3 символа");
+    }
+  };
+
+  const onAddButtonClick = () => {
+    if (!validate(value)) return;
+    setList((prev) => [...prev, { value }]);
+    setValue("");
+    setError("");
+  };
+
+  const isValueVaild = validate(value);
+
   return (
     <div className={styles.app}>
       <h1 className={styles["page-heading"]}>Ввод значения</h1>
       <p className={["no-margin-text"]}>
         Текущее значение <code>value</code>: "
-        <output className={"current-value"}></output>"
+        <output className={"current-value"}>{value}</output>"
       </p>
-      <div className={styles.error}>
-        Введенное значение должно содержать минимум 3 символа
-      </div>
+      {error !== "" && <div className={styles.error}>{error}</div>}
       <div className={styles["buttons-container"]}>
-        <button className={styles.button}>Ввести новое</button>
-        <button className={styles.button}>Добавить в список</button>
+        <button className={styles.button} onClick={onInputButtonClick}>
+          Ввести новое
+        </button>
+        <button
+          className={styles.button}
+          disabled={!isValueVaild}
+          onClick={onAddButtonClick}
+        >
+          Добавить в список
+        </button>
       </div>
       <div className={styles["list-container"]}>
         <h2 className={styles["list-heading"]}>Список:</h2>
-        <p className={styles["no-margin-text"]}>Нет добавленных элементов</p>
-        <ul className={styles.list}>
-          <li className={styles["list-item"]}></li>
-        </ul>
+        {list.length > 0 ? (
+          <ul className={styles.list}>
+            {list.map((item) => (
+              <li className={styles["list-item"]} key={item.id}>
+                {item.value}
+              </li>
+            ))}
+          </ul>
+        ) : (
+          <p className={styles["no-margin-text"]}>Нет добавленных элементов</p>
+        )}
       </div>
     </div>
   );
